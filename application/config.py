11fad0fa8af7a9a6912e935656c2c61af56ddce4
Spyros Alexakis <spyros@mint>
2024-04-01 19:10:02 +0300

    fix mail in config.py

--- application/config.py
+++ application/config.py
@@ -7,7 +7,7 @@ class DevelopmentConfig:
   SECRET_KEY = os.getenv('SECRET_KEY')
   MAIL_SERVER = 'smtp.googlemail.com'
   MAIL_PORT = 465
-  MAIL_USERNAME = 'salexakis81@gmail.com'
+  MAIL_USERNAME = os.getenv('MAIL')
   MAIL_PASSWORD = os.getenv('MAIL_PASSWORD')
   MAIL_USE_TLS = False
   MAIL_USE_SSL = True
